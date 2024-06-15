4eef4bf3a8d5b8344804ece10a2d84439da2c6a1
Melina Pashna <melinaapashna@gmail.com>
2024-06-14 23:04:50 -0700

    initial commit

---
+++ script/app.js
@@ -0,0 +1,45 @@
+// 
+// CHANNEL: https: //t.me/OpenSourcecode_pro
+
+//     CHANNEL: https: //t.me/OpenSourcecode_pro
+
+//     CHANNEL: https: //t.me/OpenSourcecode_pro
+
+//     CHANNEL: https: //t.me/OpenSourcecode_pro
+
+//     CHANNEL: https: //t.me/OpenSourcecode_pro
+
+const filterButtons = document.querySelectorAll(".filter_buttons button");
+const filterbleCards = document.querySelectorAll(".filterble_cards .card");
+
+const filterCards = e => {
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+    console.log(e.target);
+
+
+    filterbleCards.forEach(card => {
+        card.classList.add("hide");
+
+        if (card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all") {
+            card.classList.remove("hide");
+        }
+    });
+};
+
+filterButtons.forEach(button => button.addEventListener("click", filterCards));
+
+
+
+
+
+// 
+// CHANNEL : https://t.me/OpenSourcecode_pro
+
+// CHANNEL : https://t.me/OpenSourcecode_pro
+
+// CHANNEL : https://t.me/OpenSourcecode_pro
+
+// CHANNEL : https://t.me/OpenSourcecode_pro
+
+// CHANNEL : https://t.me/OpenSourcecode_pro
